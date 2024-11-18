8d9f98dd75ec283bcb347962a6fe1b0dd4a5ba64
Injun Jeong <jijun897@naver.com>
2024-11-18 20:11:03 +0900

    feat: menuActivity에 랜덤하게 크롤링한 기사 표시

--- app/src/main/java/com/example/ecocapture/MenuActivity.kt
+++ app/src/main/java/com/example/ecocapture/MenuActivity.kt
@@ -2,15 +2,15 @@ package com.example.ecocapture
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import com.example.ecocapture.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity()
 {
+    private lateinit var webCrawler: WebCrawler
     lateinit var binding: ActivityMenuBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +19,7 @@ class MenuActivity : AppCompatActivity()
         setContentView(binding.root)
 
         initEvents()
+        initNews()
     }
 
     private fun initEvents()
@@ -32,5 +33,20 @@ class MenuActivity : AppCompatActivity()
             val intent = Intent(this, TextActivity::class.java)
             startActivity(intent)
         }
+    }
+    private fun initNews() {
+        webCrawler = WebCrawler()
+
+        // WebCrawler의 LiveData를 관찰하여 데이터가 변경될 때 UI를 갱신
+        webCrawler.articlesLiveData.observe(this, Observer { articles ->
+
+            // 랜덤한 기사 선택
+            if (articles.isNotEmpty()) {
+                val randomArticle = articles.random() // 랜덤으로 하나 선택
+                binding.randomTitleTextView.text = randomArticle.first // 기사 제목 표시
+            }
+        })
+        // 기사 데이터 가져오기
+        webCrawler.fetchArticles()
     }
 }
