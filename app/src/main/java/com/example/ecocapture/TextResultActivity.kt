a0d6d01b4f2db0d406d84bc657e5b916098f090e
Injun Jeong <jijun897@naver.com>
2024-12-03 15:37:11 +0900

    fix: 이미지 검색 결과 창에서 뒤로가기 버튼 작동하도록 수정

--- app/src/main/java/com/example/ecocapture/TextResultActivity.kt
+++ app/src/main/java/com/example/ecocapture/TextResultActivity.kt
@@ -1,32 +1,38 @@
 package com.example.ecocapture
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ecocapture.databinding.ActivityTextResultBinding
 
 class TextResultActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityTextResultBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_text_result)
 
-        binding = ActivityTextResultBinding.inflate(layoutInflater)
+        val binding = ActivityTextResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // 툴바
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // UI 컴포넌트 초기화 및 레이아웃의 요소와 연결
-        val textViewResult: TextView = findViewById(R.id.textViewResult)
-
         // Intent에서 전달된 데이터 수신
         val responseText = intent.getStringExtra("responseText")
 
         // 결과를 TextView에 표시
-        textViewResult.text = responseText ?: "결과가 없습니다."
+        binding.textViewResult.text = responseText ?: "결과가 없습니다."
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                // 뒤로가기 버튼 클릭 시 현재 액티비티 종료
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
